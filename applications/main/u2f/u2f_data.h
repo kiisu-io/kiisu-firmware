2f44cc70cd7fdbf2dac2622d803fc7f77c4f4fa9
Sasha Sovenko <wintersky@runbox.com>
2026-05-22 00:00:04 +0300

    u2f: generate self-signed attestation certificate on first launch

--- applications/main/u2f/u2f_data.h
+++ applications/main/u2f/u2f_data.h
@@ -8,6 +8,16 @@ extern "C" {
 
 bool u2f_data_check(bool cert_only);
 
+/**
+ * Generate a self-signed P-256 attestation certificate (and matching private
+ * key file marked as USER_UNENCRYPTED) on the SD card, if they don't already
+ * exist. The unencrypted private key will be encrypted with the device-unique
+ * enclave key on first read by u2f_data_cert_key_load().
+ *
+ * @return true if files exist (newly created or already present) after the call.
+ */
+bool u2f_data_cert_generate_if_missing(void);
+
 bool u2f_data_cert_check(void);
 
 uint32_t u2f_data_cert_load(uint8_t* cert);
